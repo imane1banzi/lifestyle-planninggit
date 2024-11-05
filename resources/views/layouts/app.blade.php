4d2c2c788655ad10727d3507c1687eb14ae5af07
Imane Banzi <044883@intelcia.com>
2024-11-05 20:55:36 +0100

    https

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <base href="{{ config('app.url') }}/">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -14,9 +13,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        <link rel="stylesheet" href="https://lifestyle-planninggit.onrender.com/build/assets/app.css">
-        <script src="https://lifestyle-planninggit.onrender.com/build/assets/app.js" defer></script>
-        
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
