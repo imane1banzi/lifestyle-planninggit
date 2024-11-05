96cf2fee383ec5e92e0e7bf04e383fc082029447
Imane Banzi <044883@intelcia.com>
2024-11-05 20:42:50 +0100

    https

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,9 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="https://lifestyle-planninggit.onrender.com/build/assets/app.css">
+        <script src="https://lifestyle-planninggit.onrender.com/build/assets/app.js" defer></script>
+        
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
