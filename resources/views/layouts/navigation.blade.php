b6c1c880b1fbc17dbea9d7f416a6d53dec59be21
Imane Banzi <044883@INTELCIA.COM>
2024-11-07 09:21:52 +0100

    lifecss

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <img src="{{ asset('images/logolifereer.png') }}" alt="Logo de l'application" class="block h-20 w-20 fill-current text-gray-800" />
+                        <img src="{{ secure_asset('images/logolifereer.png') }}" alt="Logo de l'application" class="block h-20 w-20 fill-current text-gray-800" />
 
                         
                     </a>
