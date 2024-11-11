9e2eafdb593e1bfd33717a80c7f9eb3163ba9b44
Imane Banzi <044883@intelcia.com>
2024-11-11 16:11:37 +0100

    newlifestyle

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,15 +4,26 @@ namespace App\Http\Controllers;
 use App\Models\Profile;
 use App\Models\Expense;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class DashboardController extends Controller
 {
     public function index()
     {
-        $totalProfiles = Profile::count(); // Compte le nombre total de profils
-        $totalExpenses = Expense::sum('amount'); // Somme des montants des dépenses
-        $yearlyTotal = Expense::where('created_at', '>=', now()->subYear())->sum('amount')*12;
+     // Récupérer l'utilisateur connecté
+     $user = Auth::user();
 
-        return view('dashboard', compact('totalProfiles', 'totalExpenses', 'yearlyTotal'));
-    }
+     // Compte le nombre total de profils associés à l'utilisateur
+     $totalProfiles = Profile::where('user_id', $user->id)->count();
+
+     // Somme des montants des dépenses de l'utilisateur connecté
+     $totalExpenses = Expense::where('user_id', $user->id)->sum('amount');
+
+     // Calculer le total des dépenses de l'année écoulée et multiplier par 12 pour obtenir une estimation annuelle
+     $yearlyTotal = Expense::where('user_id', $user->id)
+                            ->where('created_at', '>=', now()->subYear())
+                            ->sum('amount') * 12;
+
+     // Retourner les résultats à la vue
+     return view('dashboard', compact('totalProfiles', 'totalExpenses', 'yearlyTotal'));
+ }
 }
