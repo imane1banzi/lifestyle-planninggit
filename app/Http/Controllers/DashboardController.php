e679b8211e61b78b37a8c2e75a7a540ca11ab076
Imane Banzi <044883@intelcia.com>
2024-11-11 16:34:28 +0100

    newlifestyle

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,27 +9,23 @@ class DashboardController extends Controller
 {
     public function index()
     {
-  // Vérifier si l'utilisateur est connecté
-  if (Auth::check()) {
-    // Récupérer l'utilisateur connecté
-    $user = Auth::user();
+  // Récupérer l'utilisateur connecté
+  $user = Auth::user();
 
-    // Compte le nombre total de profils associés à l'utilisateur
-    $totalProfiles = Profile::where('user_id', $user->id)->count();
+  // Vérifiez si l'utilisateur est authentifié
+  if ($user) {
+      // Si l'utilisateur est connecté, récupérer les données
+      $totalProfiles = Profile::where('user_id', $user->id)->count();
+      $totalExpenses = Expense::where('user_id', $user->id)->sum('amount');
+      $yearlyTotal = Expense::where('user_id', $user->id)
+                          ->where('created_at', '>=', now()->subYear())
+                          ->sum('amount') * 12;
 
-    // Somme des montants des dépenses de l'utilisateur connecté
-    $totalExpenses = Expense::where('user_id', $user->id)->sum('amount');
-
-    // Calculer le total des dépenses de l'année écoulée et multiplier par 12 pour obtenir une estimation annuelle
-    $yearlyTotal = Expense::where('user_id', $user->id)
-                           ->where('created_at', '>=', now()->subYear())
-                           ->sum('amount') * 12;
-
-    // Retourner les résultats à la vue pour un utilisateur connecté
-    return view('dashboard', compact('totalProfiles', 'totalExpenses', 'yearlyTotal'));
-} else {
-    // Si l'utilisateur n'est pas connecté, retourner la vue pour les invités
-    return view('guest.dashboard'); // Assurez-vous que cette vue existe
-}
+      // Retourner les résultats à la vue
+      return view('dashboard', compact('totalProfiles', 'totalExpenses', 'yearlyTotal'));
+  } else {
+      // Si l'utilisateur n'est pas connecté, ne renvoyer aucune donnée supplémentaire
+      return view('dashboard');
+  }
 }
 }
