70cc7161008c36fcb2f70e0e83aa0d9623372c3c
Imane Banzi <044883@intelcia.com>
2024-11-11 16:17:35 +0100

    newlifestyle

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,21 +9,27 @@ class DashboardController extends Controller
 {
     public function index()
     {
-     // Récupérer l'utilisateur connecté
-     $user = Auth::user();
+  // Vérifier si l'utilisateur est connecté
+  if (Auth::check()) {
+    // Récupérer l'utilisateur connecté
+    $user = Auth::user();
 
-     // Compte le nombre total de profils associés à l'utilisateur
-     $totalProfiles = Profile::where('user_id', $user->id)->count();
+    // Compte le nombre total de profils associés à l'utilisateur
+    $totalProfiles = Profile::where('user_id', $user->id)->count();
 
-     // Somme des montants des dépenses de l'utilisateur connecté
-     $totalExpenses = Expense::where('user_id', $user->id)->sum('amount');
+    // Somme des montants des dépenses de l'utilisateur connecté
+    $totalExpenses = Expense::where('user_id', $user->id)->sum('amount');
 
-     // Calculer le total des dépenses de l'année écoulée et multiplier par 12 pour obtenir une estimation annuelle
-     $yearlyTotal = Expense::where('user_id', $user->id)
-                            ->where('created_at', '>=', now()->subYear())
-                            ->sum('amount') * 12;
+    // Calculer le total des dépenses de l'année écoulée et multiplier par 12 pour obtenir une estimation annuelle
+    $yearlyTotal = Expense::where('user_id', $user->id)
+                           ->where('created_at', '>=', now()->subYear())
+                           ->sum('amount') * 12;
 
-     // Retourner les résultats à la vue
-     return view('dashboard', compact('totalProfiles', 'totalExpenses', 'yearlyTotal'));
- }
+    // Retourner les résultats à la vue pour un utilisateur connecté
+    return view('dashboard', compact('totalProfiles', 'totalExpenses', 'yearlyTotal'));
+} else {
+    // Si l'utilisateur n'est pas connecté, retourner la vue pour les invités
+    return view('guest.dashboard'); // Assurez-vous que cette vue existe
+}
+}
 }
